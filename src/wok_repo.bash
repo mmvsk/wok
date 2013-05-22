ab88d0418b10bb66fee20389cbbdea67bafbad88
Max Ruman <m@rmx.cc>
2013-05-22 16:20:28 +0200

    Wok repo interface

--- src/wok_repo.bash
+++ src/wok_repo.bash
@@ -18,64 +18,216 @@
 # License along with Wok. If not, see <http://www.gnu.org/licenses/>.
 #
 
-#
-# Uses
-# ----------------------------
-# 
-# json_set <jsonFile_path> <key> <value>
-# json_get <jsonFile_path> <key>
-#
-# index_has <index_path> <token>
-# index_add <index_path> <token>
-# index_remove <index_path> <token>
-#
-# Directory structure
-# ------------------------------------------
-#
-# $repo/domain.index
-# $repo/modules/<module>/<domain>.ini
-# $repo/modules/<module>/index/<index>.index
-#
-# Functions
-# -----------------------------------------------------------
+WOK_REPO_DOMAIN_INDEX="${WOK_REPO_PATH}/domain.index"
+
 #
 # Usage: wok_repo_has <domain>
-# Usage: wok_repo_add <domain>
-# Usage: wok_repo_remove <domain>
-# Usage: wok_repo_list <domain>
 #
-# Usage: wok_repo_module_has <module> <domain>
-# Usage: wok_repo_module_add <module> <domain>
-# Usage: wok_repo_module_remove <module> <domain>
-# Usage: wok_repo_module_list <module> <domain>
+# @param string $domain Domain name
+# @return bool Existence
 #
-# Usage: wok_repo_module_set <module> <domain> <key> <value>
-# Usage: wok_repo_module_get <module> <domain> <key>
-#
-# Usage: wok_repo_module_index_has <module> <index> <name>
-# Usage: wok_repo_module_index_add <module> <index> <name>
-# Usage: wok_repo_module_index_remove <module> <index> <name>
-# Usage: wok_repo_module_index_list <module> <index>
-# Usage: wok_repo_module_index_getPath <module> <index>
-#
-# WOK_REPO_PATH
-#
-
 wok_repo_has()
 {
 	local domain="$1"
 
-	return 1
+	index_has "$WOK_REPO_DOMAIN_INDEX" "$domain"
 }
 
+#
+# Usage: wok_repo_add <domain>
+#
+# @param string $domain Domain name
+# @return bool Success
+#
 wok_repo_add()
 {
 	local domain="$1"
+
+	index_add "$WOK_REPO_DOMAIN_INDEX" "$domain"
 }
 
-wok_repo_cust_has()
+#
+# Usage: wok_repo_remove <domain>
+#
+# @param string $domain Domain name
+# @return bool Success
+#
+wok_repo_remove()
 {
-	local module="$1"
-	local section="$2"
-	local token="$3"
+	local domain="$1"
+
+	index_remove "$WOK_REPO_DOMAIN_INDEX" "$domain"
+}
+
+#
+# Usage: wok_repo_list
+#
+# @print Domain list
+#
+wok_repo_list()
+{
+	cat "$WOK_REPO_DOMAIN_INDEX"
+}
+
+#
+# Usage: wok_repo_module_has <module> <domain>
+#
+wok_repo_module_has()
+{
+	# index_has <index_path> <token>
+	# index_add <index_path> <token>
+	# index_remove <index_path> <token>
+	#
+	# $repo/modules/<module>/<domain>.ini
+	# $repo/modules/<module>/index/<index>.index
+	#
+	# $WOK_REPO_PATH
+}
+
+#
+# Usage: wok_repo_module_add <module> <domain>
+#
+wok_repo_module_add()
+{
+	# index_has <index_path> <token>
+	# index_add <index_path> <token>
+	# index_remove <index_path> <token>
+	#
+	# $repo/modules/<module>/<domain>.ini
+	# $repo/modules/<module>/index/<index>.index
+	#
+	# $WOK_REPO_PATH
+}
+
+#
+# Usage: wok_repo_module_remove <module> <domain>
+#
+wok_repo_module_remove()
+{
+	# index_has <index_path> <token>
+	# index_add <index_path> <token>
+	# index_remove <index_path> <token>
+	#
+	# $repo/modules/<module>/<domain>.ini
+	# $repo/modules/<module>/index/<index>.index
+	#
+	# $WOK_REPO_PATH
+}
+
+#
+# Usage: wok_repo_module_list <module> <domain>
+#
+wok_repo_module_list()
+{
+	# index_has <index_path> <token>
+	# index_add <index_path> <token>
+	# index_remove <index_path> <token>
+	#
+	# $repo/modules/<module>/<domain>.ini
+	# $repo/modules/<module>/index/<index>.index
+	#
+	# $WOK_REPO_PATH
+}
+
+#
+# Usage: wok_repo_module_data_set <module> <domain> <key> <value>
+#
+wok_repo_module_data_set()
+{
+	# json_set <jsonFile_path> <key> <value>
+	# json_get <jsonFile_path> <key>
+	#
+	# $repo/modules/<module>/<domain>.ini
+	# $repo/modules/<module>/index/<index>.index
+	#
+	# $WOK_REPO_PATH
+}
+
+#
+# Usage: wok_repo_module_get <module> <domain> <key>
+#
+wok_repo_module_data_get()
+{
+	# json_set <jsonFile_path> <key> <value>
+	# json_get <jsonFile_path> <key>
+	#
+	# $repo/modules/<module>/<domain>.ini
+	# $repo/modules/<module>/index/<index>.index
+	#
+	# $WOK_REPO_PATH
+}
+
+#
+# Usage: wok_repo_module_index_has <module> <index> <token>
+#
+wok_repo_module_index_has()
+{
+	# index_has <index_path> <token>
+	# index_add <index_path> <token>
+	# index_remove <index_path> <token>
+	#
+	# $repo/modules/<module>/<domain>.ini
+	# $repo/modules/<module>/index/<index>.index
+	#
+	# $WOK_REPO_PATH
+}
+
+#
+# Usage: wok_repo_module_index_add <module> <index> <token>
+#
+wok_repo_module_index_add()
+{
+	# index_has <index_path> <token>
+	# index_add <index_path> <token>
+	# index_remove <index_path> <token>
+	#
+	# $repo/modules/<module>/<domain>.ini
+	# $repo/modules/<module>/index/<index>.index
+	#
+	# $WOK_REPO_PATH
+}
+
+#
+# Usage: wok_repo_module_index_remove <module> <index> <token>
+#
+wok_repo_module_index_remove()
+{
+	# index_has <index_path> <token>
+	# index_add <index_path> <token>
+	# index_remove <index_path> <token>
+	#
+	# $repo/modules/<module>/<domain>.ini
+	# $repo/modules/<module>/index/<index>.index
+	#
+	# $WOK_REPO_PATH
+}
+
+#
+# Usage: wok_repo_module_index_list <module> <index>
+#
+wok_repo_module_index_list()
+{
+	# index_has <index_path> <token>
+	# index_add <index_path> <token>
+	# index_remove <index_path> <token>
+	#
+	# $repo/modules/<module>/<domain>.ini
+	# $repo/modules/<module>/index/<index>.index
+	#
+	# $WOK_REPO_PATH
+}
+
+#
+# Usage: wok_repo_module_index_getPath <module> <index>
+#
+wok_repo_module_index_getPath()
+{
+	# index_has <index_path> <token>
+	# index_add <index_path> <token>
+	# index_remove <index_path> <token>
+	#
+	# $repo/modules/<module>/<domain>.ini
+	# $repo/modules/<module>/index/<index>.index
+	#
+	# $WOK_REPO_PATH
 }
