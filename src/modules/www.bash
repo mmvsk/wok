0e406247d1ea9ac7e5a1a84673f9569eb2715780
Max Ruman <max@mvsk.io>
2024-05-13 13:26:06 +0200

    remove php

--- src/modules/www.bash
+++ src/modules/www.bash
@@ -43,19 +43,19 @@ wok_www_pusage()
 	echo
 	echo "        Usage: ~ [--force|-f] <domain>"
 	echo
-	echo "    create-ssl  Create an SSL certificate request for the given domain;"
-	echo "                This command can only be called with interactive mode"
-	echo
-	echo "        Usage: ~ --interactive|-i [--force|-f] <domain>"
-	echo
+	#echo "    create-ssl  Create an SSL certificate request for the given domain;"
+	#echo "                This command can only be called with interactive mode"
+	#echo
+	#echo "        Usage: ~ --interactive|-i [--force|-f] <domain>"
+	#echo
 	echo "    edit-nginx  Edit nginx virtual host configuration using ${EDITOR}"
 	echo
 	echo "        Usage: ~ <domain>"
 	echo
-	echo "    edit-php    Edit PHP FPM pool configuration using ${EDITOR}"
-	echo
-	echo "        Usage: ~ <domain>"
-	echo
+	#echo "    edit-php    Edit PHP FPM pool configuration using ${EDITOR}"
+	#echo
+	#echo "        Usage: ~ <domain>"
+	#echo
 	echo "    su          Log-in as the specififed user"
 	echo
 	echo "        Usage: ~ <domain>"
@@ -85,10 +85,10 @@ wok_www_add()
 	local nginx_vhost_conf_dir="$(wok_config_get wok_www nginx_vhost_conf_dir)"
 	local nginx_daemon_command_reload=$(wok_config_get wok_www nginx_daemon_command_reload)
 	local nginx_vhost_conf_path
-	local php_fpm_pool_template="$(wok_config_get wok_www php_fpm_pool_template)"
-	local php_fpm_pool_dir="$(wok_config_get wok_www php_fpm_pool_dir)"
-	local php_daemon_command_reload=$(wok_config_get wok_www php_daemon_command_reload)
-	local php_fpm_pool_path
+	#local php_fpm_pool_template="$(wok_config_get wok_www php_fpm_pool_template)"
+	#local php_fpm_pool_dir="$(wok_config_get wok_www php_fpm_pool_dir)"
+	#local php_daemon_command_reload=$(wok_config_get wok_www php_daemon_command_reload)
+	#local php_fpm_pool_path
 
 	if ! wok_repo_has "$domain"; then
 		wok_perror "Domain '${domain}' is not managed by Wok."
@@ -120,17 +120,17 @@ wok_www_add()
 		wok_perror "Nginx vhost configuration directory '${nginx_vhost_conf_dir}' does not exist or is not writable."
 		wok_exit $EXIT_ERR_SYS
 	fi
-	if [[ ! -d "$php_fpm_pool_dir" || ! -w "$php_fpm_pool_dir" ]]; then
-		wok_perror "PHP FPM pool directory '${php_fpm_pool_dir}' does not exist or is not writable."
-		wok_exit $EXIT_ERR_SYS
-	fi
+	#if [[ ! -d "$php_fpm_pool_dir" || ! -w "$php_fpm_pool_dir" ]]; then
+		#wok_perror "PHP FPM pool directory '${php_fpm_pool_dir}' does not exist or is not writable."
+		#wok_exit $EXIT_ERR_SYS
+	#fi
 
 	# Generate paths
 	home_path="${home_path_base}/${uid}"
 	home_passwd_path="${home_path}/${home_passwd_file}"
 	www_path="${www_path_base}/${domain}"
 	nginx_vhost_conf_path="${nginx_vhost_conf_dir}/${domain}.conf"
-	php_fpm_pool_path="${php_fpm_pool_dir}/${uid}.conf"
+	#php_fpm_pool_path="${php_fpm_pool_dir}/${uid}.conf"
 
 	# Verify templates existence
 	if [[ ! -e "$home_template" ]]; then
@@ -145,10 +145,10 @@ wok_www_add()
 		wok_perror "Nginx vhost configuration template '${nginx_vhost_conf_template}' does not exist."
 		wok_exit $EXIT_ERR_SYS
 	fi
-	if [[ ! -e "$php_fpm_pool_template" ]]; then
-		wok_perror "PHP FPM pool template '${php_fpm_pool_template}' does not exist."
-		wok_exit $EXIT_ERR_SYS
-	fi
+	#if [[ ! -e "$php_fpm_pool_template" ]]; then
+		#wok_perror "PHP FPM pool template '${php_fpm_pool_template}' does not exist."
+		#wok_exit $EXIT_ERR_SYS
+	#fi
 
 	# Verify paths availability
 	if [[ -e $home_path ]]; then
@@ -163,10 +163,10 @@ wok_www_add()
 		wok_perror "Nginx vhost configuration file '${nginx_vhost_conf_path}' already exists."
 		wok_exit $EXIT_ERR_SYS
 	fi
-	if [[ -e $php_fpm_pool_path ]]; then
-		wok_perror "PHP FPM pool file '${php_fpm_pool_path}' already exists."
-		wok_exit $EXIT_ERR_SYS
-	fi
+	#if [[ -e $php_fpm_pool_path ]]; then
+		#wok_perror "PHP FPM pool file '${php_fpm_pool_path}' already exists."
+		#wok_exit $EXIT_ERR_SYS
+	#fi
 
 	# Create system user
 	if ! useradd -g "$user_gid" -d "$home_path" -s "$user_shell" "$uid"; then
@@ -204,9 +204,9 @@ wok_www_add()
 	sed -i "s/{uid}/${uid}/g" "$nginx_vhost_conf_path"
 
 	# Configure PHP
-	cp "$php_fpm_pool_template" "$php_fpm_pool_path"
-	sed -i "s/{domain}/${domain}/g" "$php_fpm_pool_path"
-	sed -i "s/{uid}/${uid}/g" "$php_fpm_pool_path"
+	#cp "$php_fpm_pool_template" "$php_fpm_pool_path"
+	#sed -i "s/{domain}/${domain}/g" "$php_fpm_pool_path"
+	#sed -i "s/{uid}/${uid}/g" "$php_fpm_pool_path"
 
 	# Register...
 	wok_repo_module_add "www" "$domain"
@@ -214,7 +214,7 @@ wok_www_add()
 	wok_repo_module_data_set "www" "$domain" "uid" "$uid"
 
 	# Reload PHP and Nginx daemons
-	$php_daemon_command_reload
+	#$php_daemon_command_reload
 	$nginx_daemon_command_reload
 }
 
@@ -249,8 +249,8 @@ wok_www_remove()
 	www_path="$(wok_config_get wok_www www_path_base)/${domain}"
 	local nginx_vhost_conf_path="$(wok_config_get wok_www nginx_vhost_conf_dir)/${domain}.conf"
 	local nginx_daemon_command_reload=$(wok_config_get wok_www nginx_daemon_command_reload)
-	local php_fpm_pool_path="$(wok_config_get wok_www php_fpm_pool_dir)/${uid}.conf"
-	local php_daemon_command_reload=$(wok_config_get wok_www php_daemon_command_reload)
+	#local php_fpm_pool_path="$(wok_config_get wok_www php_fpm_pool_dir)/${uid}.conf"
+	#local php_daemon_command_reload=$(wok_config_get wok_www php_daemon_command_reload)
 
 	if ! egrep -q "^${uid}:" /etc/passwd; then
 		wok_perror "System user '${uid}' does not exist on this host."
@@ -269,18 +269,18 @@ wok_www_remove()
 		wok_perror "Nginx vhost configuration file '${nginx_vhost_conf_path}' does not exist or is not deletable (writable)"
 		wok_exit $EXIT_ERR_SYS
 	fi
-	if [[ ! -f $php_fpm_pool_path || ! -w $php_fpm_pool_path ]]; then
-		wok_perror "PHP FPM pool file '${php_fpm_pool_path}' does not exist or is not deletable (writable)"
-		wok_exit $EXIT_ERR_SYS
-	fi
+	#if [[ ! -f $php_fpm_pool_path || ! -w $php_fpm_pool_path ]]; then
+		#wok_perror "PHP FPM pool file '${php_fpm_pool_path}' does not exist or is not deletable (writable)"
+		#wok_exit $EXIT_ERR_SYS
+	#fi
 
 	# Remove Nginx and PHP configuration files
 	rm -f "$nginx_vhost_conf_path"
-	rm -f "$php_fpm_pool_path"
+	#rm -f "$php_fpm_pool_path"
 
 	# Reload PHP and Nginx daemons
 	$nginx_daemon_command_reload
-	$php_daemon_command_reload
+	#$php_daemon_command_reload
 
 	# Remove system user
 	if ! userdel -f "$uid"; then
@@ -574,19 +574,19 @@ wok_www_handle()
 			ui_showProgress "Unbinding domain '${domain}' from 'www' module" "${cmd[@]}"
 			return $?;;
 
-		create-ssl)
-			if [[ ${#args_remain[@]} -ne 1 ]]; then
-				wok_perror "Invalid usage. See '${WOK_COMMAND} www --help'."
-				wok_exit $EXIT_ERR_USR
-			fi
-			array_shift args_remain domain || wok_exit $EXIT_ERR_SYS
+		#create-ssl)
+			#if [[ ${#args_remain[@]} -ne 1 ]]; then
+				#wok_perror "Invalid usage. See '${WOK_COMMAND} www --help'."
+				#wok_exit $EXIT_ERR_USR
+			#fi
+			#array_shift args_remain domain || wok_exit $EXIT_ERR_SYS
 
-			if ! $interactive; then
-				wok_perror "Can't create an SSL certificate signing request in non-interactive mode"
-				wok_exit $EXIT_ERR_USR
-			fi
+			#if ! $interactive; then
+				#wok_perror "Can't create an SSL certificate signing request in non-interactive mode"
+				#wok_exit $EXIT_ERR_USR
+			#fi
 
-			wok_www_createSsl  "$domain" "$force";;
+			#wok_www_createSsl  "$domain" "$force";;
 
 		edit-nginx)
 			if [[ ${#args_remain[@]} -ne 1 ]]; then
@@ -606,24 +606,24 @@ wok_www_handle()
 			$EDITOR "${nginx_vhost_conf_dir}/${domain}.conf" -c "setf nginx"
 			ui_showProgress "Reloading nginx" $nginx_daemon_command_reload;;
 
-		edit-php)
-			if [[ ${#args_remain[@]} -ne 1 ]]; then
-				wok_perror "Invalid usage. See '${WOK_COMMAND} www --help'."
-				wok_exit $EXIT_ERR_USR
-			fi
-			array_shift args_remain domain || wok_exit $EXIT_ERR_SYS
+		#edit-php)
+			#if [[ ${#args_remain[@]} -ne 1 ]]; then
+				#wok_perror "Invalid usage. See '${WOK_COMMAND} www --help'."
+				#wok_exit $EXIT_ERR_USR
+			#fi
+			#array_shift args_remain domain || wok_exit $EXIT_ERR_SYS
 
-			if ! wok_www_has "$domain"; then
-				wok_perror "Domain '${domain}' is not bound to 'www' module."
-				wok_exit $EXIT_ERR_USR
-			fi
+			#if ! wok_www_has "$domain"; then
+				#wok_perror "Domain '${domain}' is not bound to 'www' module."
+				#wok_exit $EXIT_ERR_USR
+			#fi
 
-			local uid="$(wok_www_getUid "$domain")"
-			local php_fpm_pool_dir="$(wok_config_get wok_www php_fpm_pool_dir)"
-			local php_daemon_command_reload=$(wok_config_get wok_www php_daemon_command_reload)
+			#local uid="$(wok_www_getUid "$domain")"
+			#local php_fpm_pool_dir="$(wok_config_get wok_www php_fpm_pool_dir)"
+			#local php_daemon_command_reload=$(wok_config_get wok_www php_daemon_command_reload)
 
-			$EDITOR "${php_fpm_pool_dir}/${uid}.conf" -c "setf dosini"
-			ui_showProgress "Reloading PHP" $php_daemon_command_reload;;
+			#$EDITOR "${php_fpm_pool_dir}/${uid}.conf" -c "setf dosini"
+			#ui_showProgress "Reloading PHP" $php_daemon_command_reload;;
 
 		su)
 			if [[ ${#args_remain[@]} -ne 1 ]]; then
